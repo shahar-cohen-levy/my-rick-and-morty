01cc0c531bfca221293ae9a94233fa0810296ad0
Shahar Cohen <shaharcoh@gmail.com>
2019-09-05 16:13:01 +0100

    adding apollo to use graphql

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Root from './Root';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const httpLink = createHttpLink({
+    uri: 'https://rickandmortyapi.com/graphql/'
+  })
+
+  const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+    defaultOptions: {
+      query: {
+        fetchPolicy: 'network-only',
+        errorPolicy: 'all',
+      },
+    }
+  })
+
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+      <Root />
+    </ApolloProvider>,
+    document.getElementById('root')
+  )
+  serviceWorker.unregister();
+
+
